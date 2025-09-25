463e5e1a829d766d07c075e2de105f7f487ff851
Thiago <thiago.correa9@fatec.sp.gov.br>
2025-09-24 22:04:24 -0300

    Add files via upload

---
+++ Atividade6/operacoes.js
@@ -0,0 +1,17 @@
+let num1 = parseFloat(prompt("Digite o primeiro número:"));
+let num2 = parseFloat(prompt("Digite o segundo número:"));
+
+let soma = num1 + num2;
+let subtracao = num1 - num2;
+let produto = num1 * num2;
+let divisao = num1 / num2;
+let resto = num1 % num2;
+
+alert(
+    "Resultados:\n" +
+    "Soma: " + soma + "\n" +
+    "Subtração: " + subtracao + "\n" +
+    "Produto: " + produto + "\n" +
+    "Divisão: " + divisao.toFixed(2) + "\n" +
+    "Resto: " + resto
+);
